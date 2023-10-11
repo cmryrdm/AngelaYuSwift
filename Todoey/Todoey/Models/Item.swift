74fe7581607ca8211f1e9a16079ca2a904feaf52
Cemre YARDIM <cemreyardim@gmail.com>
2023-10-11 19:13:21 +0300

    realm data models added

--- Todoey/Todoey/Models/Item.swift
+++ Todoey/Todoey/Models/Item.swift
@@ -9,5 +9,7 @@ import Foundation
 import RealmSwift
 
 class Item: Object {
-  @objc dynamic var name: String = ""
+  @objc dynamic var title: String = ""
+  @objc dynamic var done: Bool = false
+  var parentCategory = LinkingObjects(fromType: Category.self, property: "items") // inverse relationship
 }
